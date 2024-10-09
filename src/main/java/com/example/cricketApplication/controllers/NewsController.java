eac5bb883b50ae897988ea7c67c89268bae16acf
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2024-10-09 15:43:35 +0530

    added the under to the player response

--- src/main/java/com/example/cricketApplication/controllers/NewsController.java
+++ src/main/java/com/example/cricketApplication/controllers/NewsController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/api/news")
