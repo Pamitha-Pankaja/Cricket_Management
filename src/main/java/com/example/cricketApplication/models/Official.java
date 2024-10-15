dec9ee8e42a59c86315996fb5342ea4d359c1832
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2024-10-15 09:10:38 +0530

    Created the crud operations and signUp for Officials

---
+++ src/main/java/com/example/cricketApplication/models/Official.java
@@ -0,0 +1,41 @@
+package com.example.cricketApplication.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "officials")
+public class Official {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+
+    @NotBlank
+    @Size(max = 15)
+    private String contactNo;
+
+    @NotBlank
+    @Size(max = 100)
+    private String position;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+}
+
