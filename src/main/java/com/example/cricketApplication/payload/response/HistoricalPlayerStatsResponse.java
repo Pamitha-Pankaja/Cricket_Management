fbdc7dc085301bfe0edc1cb12ba2cdd50c877d62
YERAN0105 <yeranfernando2002@gmail.com>
2024-11-01 11:21:36 +0530

    added the response class for the historicalPlayerStats

---
+++ src/main/java/com/example/cricketApplication/payload/response/HistoricalPlayerStatsResponse.java
@@ -0,0 +1,29 @@
+package com.example.cricketApplication.payload.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+public class HistoricalPlayerStatsResponse {
+    private Long id;
+    private String inning;
+    private int runs;
+    private int wickets;
+    private int fours;
+    private int sixers;
+    private int fifties;
+    private int centuries;
+    private int balls;
+    private BigDecimal overs;
+    private int runsConceded;
+    private String matchType;
+    private Long playerId;  // Assuming player ID is enough for the response
+    private String createdBy;
+    private Date createdOn;
+    private String updatedBy;
+    private Date updatedOn;
+
+}
